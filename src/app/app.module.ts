d5867db612ad99a9e14690cc747181c391907d59
josephntcha <kouagoujosephn@gmail.com>
2024-11-05 18:40:41 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
+import { en_US, fr_FR } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import fr from '@angular/common/locales/fr';
 
 registerLocaleData(en);
 
