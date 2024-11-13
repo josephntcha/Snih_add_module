a927d1a08309d1a7657a8523b9fc65daf7587327
josephntcha <kouagoujosephn@gmail.com>
2024-11-13 10:15:47 +0100

    update7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './core/auth/landing-page/landing-page.component';
+import { LandingPageComponent } from './front-office/landing-page/landing-page.component';
 import { LoginComponent } from './core/auth/login/login.component';
 
 const routes: Routes = [
   {path: "", component: LandingPageComponent},
+  { path: 'front-office', loadChildren: () => import('./front-office/front-office.module').then(m => m.FrontOfficeModule) },
 
   // {path: "login", component: LoginComponent}
 ];
