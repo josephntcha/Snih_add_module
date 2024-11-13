320ca39710245901294744989ebda3a9a0f7d6fb
DSKJ <dansoukodjo@yahoo.com>
2024-11-13 10:17:58 +0100

    update 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageComponent } from './auth/landing-page/landing-page.component';
-import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {
