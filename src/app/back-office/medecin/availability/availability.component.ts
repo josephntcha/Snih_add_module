8c045ab3e23b35f86c222b13581162a23bae447f
DSKJ <dansoukodjo@yahoo.com>
2024-12-29 13:44:03 +0100

    responsivité 1

--- src/app/back-office/medecin/availability/availability.component.ts
+++ src/app/back-office/medecin/availability/availability.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { jwtDecode } from 'jwt-decode';
 import Swal from 'sweetalert2';
 import { ApiServiceService } from '../../../services/api-service.service';
 import { AuthService } from '../../../services/auth.service';
-import { Permission, Building } from '../../../models/model';
+import { Permission } from '../../../models/model';
 
 @Component({
   selector: 'app-availability',
